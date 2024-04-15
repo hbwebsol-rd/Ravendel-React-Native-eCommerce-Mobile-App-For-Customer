10a16bc59906659688a0ab448e1a198be697d287
husain Bandookwala <husainali72@gmail.com>
2024-04-15 13:25:26 +0530

    signup password validation

--- app/screens/checkout/validationSchema.js
+++ app/screens/checkout/validationSchema.js
@@ -1,9 +1,16 @@
 import * as yup from 'yup';
 
+const regularExpression =
+  /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+const regexsmallet = /[a-z]/;
+const regexcaps = /[A-Z]/;
+const regexdigi = /[0-9]/;
+const regexspeChar = /[!@#\$%\^&\*_]/;
 const phoneReg =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 const pincodeReg = '^[0-9]{5,10}$';
-
+const PASSWORD_MSG =
+  'Password Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character';
 export const validationSchema = yup.object().shape({
   firstname: yup.string().label('First Name').min(4).required(),
   lastname: yup.string().label('Last Name').min(2).required(),
@@ -31,11 +38,15 @@ export const signupValidationSchema = yup.object().shape({
   lastName: yup.string().label('Last Name').min(2).required(),
   email: yup.string().label('Email').email().required(),
   password: yup
-    .string()
-    .label('Password')
-    .required()
-    .min(2, 'Seems a bit short...')
-    .max(10, 'We prefer insecure system, try a shorter password.'),
+    .string('')
+    .required('Please enter password')
+    .min(8, PASSWORD_MSG)
+    .required('Password is required')
+    .matches(regexsmallet, PASSWORD_MSG)
+    .matches(regexcaps, PASSWORD_MSG)
+    .matches(regexdigi, PASSWORD_MSG)
+    .matches(regexspeChar, PASSWORD_MSG)
+    .matches(regularExpression, 'Password too weak'),
   confirmPassword: yup
     .string()
     .required()
