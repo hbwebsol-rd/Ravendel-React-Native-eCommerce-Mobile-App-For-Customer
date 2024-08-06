673b15dde9a6bf714cad9d2a5732ac7ed9426dbd
mohammed maheshwar <mohammad.maheshwar@hbwebsol.com>
2024-08-06 17:17:04 +0530

    remove unused files

--- app/utils/baseurl.js
+++
@@ -1,6 +0,0 @@
-// const URL = `https://revendal-image.s3.amazonaws.com/`;
-const URL = `https://demo1-ravendel.hbwebsol.com/`;
-// export const loginURL = 'http://192.168.1.14:8000/';
-export const loginURL = 'https://demo1-ravendel.hbwebsol.com/';
-
-export default URL;
